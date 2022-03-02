1ed99d24cc677528c0875378cafe47410297d322
Noah Ruiz <88412646+noahruiz416@users.noreply.github.com>
2022-03-02 09:54:16 -0700

    Create SQL_QUERY.sql

---
+++ SQL_QUERY.sql
@@ -0,0 +1,9 @@
+## Simple SQL QUERY, used to filter dataset 
+
+SELECT Year, Quarter, Month, DayOfMonth, DayOfWeek, FlightDate, Reporting_Airline,
+ORIGIN, OriginCityName, OriginState, DestState, DestStateName, DepDelay, DepDelayMinutes, WheelsOn, TaxiOut,
+TaxiIn, ArrDelay, ArrDelayminutes, Cancelled, Distance, CarrierDelay, WeatherDelay, NASDelay, SecurityDelay, LateAircraftDelay
+FROM `tribal-facility-342503.airline_data.AIRLINE`
+WHERE ORIGIN LIKE "%PHX%" AND ArrDelay > 0 AND Year >= 2003
+OR ORIGIN LIKE "%TUS%" AND ArrDelay > 0 AND Year >= 2003
+ORDER BY Year;
